2a236bb4c69b6d96271dc0462527608b7525b1ea
nicholasmillani <nicholas.millani@sou.inteli.edu.br>
2025-05-19 09:10:15 -0300

    Seguindo a aula do cristiano

--- routes/cursos.js
+++ routes/cursos.js
@@ -5,3 +5,9 @@ const controller = require('../controllers/cursoController');
 router.post('/', controller.create);
 
 module.exports = router;
+
+// Rota para enviar o formulário de edição de curso ao controller
+router.post('/edit/:id', controller.update);
+
+// Rota para enviar o pedido de exclusão ao controller
+router.post('/delete/:id', controller.delete);
